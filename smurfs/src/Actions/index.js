ab5cb1bb549b158a56a9586c72df1afa23691064
Michael Sanchez <michaelsanchez42@live.com>
2020-06-01 21:34:12 -0600

    no mvp again

--- smurfs/src/Actions/index.js
+++ smurfs/src/Actions/index.js
@@ -7,8 +7,9 @@ export const FETCHING_SMURF_FAILURE = "FETCHING_SMURF_START"
 export const getSmurfs = () => dispatch => {
     dispatch({ type: FETCHING_SMURF_START });
 
-    axios
-    .get("http://localhost:3333/smurfs")
-    .then()
-    .catch()
+    return axios.post('/smurfs')
+    .then(res => {
+        const postedSmurfInAction = { type: FETCHING_SMURF_SUCCESS, payload: res.data }
+        dispatch(postedSmurfInAction);
+    })
 }
